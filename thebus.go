f407f5e7bad600196abac3ab31b05b70c5cc5678
Sean Nakamura <seanenakamura@gmail.com>
2020-11-19 20:52:33 -0800

    Convert response from xml to json

--- thebus.go
+++ thebus.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	xj "github.com/basgys/goxml2json"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const (
 	API_KEY = ""
 )
 
-func fetchArrivals(s string) {
+func fetchArrivals(s string) ([]byte, error) {
 	arrivalsPath := fmt.Sprintf("http://api.thebus.org/arrivals/?key=%v&stop=%v", API_KEY, s)
-	fmt.Printf(arrivalsPath)
+	fmt.Printf("%s \n", arrivalsPath)
 	resp, err := http.Get(arrivalsPath)
 	if err != nil {
 	}
-	fmt.Print(resp)
+	defer resp.Body.Close()
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+	}
+	respString := string(respData)
+	xmlString := strings.NewReader(respString)
+	jsonResp, err := xj.Convert(xmlString)
+	if err != nil {
+	}
+	r, _ := json.Marshal(jsonResp.String())
+	return r, nil
 }
