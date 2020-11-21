a8c2cf7785cfaae2155051d3dcf11f2a8eb5e909
Sean Nakamura <seanenakamura@gmail.com>
2020-11-20 23:57:52 -0800

    Return an Arrival struct for fetchArrivals

--- thebus.go
+++ thebus.go
@@ -5,29 +5,66 @@ import (
 	"fmt"
 	xj "github.com/basgys/goxml2json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
 
-const (
-	API_KEY = ""
+var (
+	apiKey = ""
 )
 
-func fetchArrivals(s string) ([]byte, error) {
-	arrivalsPath := fmt.Sprintf("http://api.thebus.org/arrivals/?key=%v&stop=%v", API_KEY, s)
+type Arrivals struct {
+	StopTimes struct {
+		Timestamp string `json:"timestamp"`
+		Arrival   []struct {
+			Headsign  string `json:"headsign"`
+			Vehicle   string `json:"vehicle"`
+			StopTime  string `json:"stopTime"`
+			Estimated string `json:"estimated"`
+			Latitude  string `json:"latitude"`
+			Shape     string `json:"shape"`
+			ID        string `json:"id"`
+			Trip      string `json:"trip"`
+			Canceled  string `json:"canceled"`
+			Date      string `json:"date"`
+			Longitude string `json:"longitude"`
+			Route     string `json:"route"`
+			Direction string `json:"direction"`
+		} `json:"arrival"`
+		Stop string `json:"stop"`
+	} `json:"stopTimes"`
+}
+
+func fetchArrivals(s string) (*Arrivals, error) {
+
+	arrivalsPath := fmt.Sprintf("http://api.thebus.org/arrivals/?key=%v&stop=%v", apiKey, s)
 	fmt.Printf("%s \n", arrivalsPath)
+
 	resp, err := http.Get(arrivalsPath)
 	if err != nil {
 	}
 	defer resp.Body.Close()
+
 	respData, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 	}
+
 	respString := string(respData)
 	xmlString := strings.NewReader(respString)
+
+	arrivals := &Arrivals{}
+
 	jsonResp, err := xj.Convert(xmlString)
+	// fmt.Printf("%v", jsonResp)
 	if err != nil {
 	}
-	r, _ := json.Marshal(jsonResp.String())
-	return r, nil
+
+	if err := json.Unmarshal(jsonResp.Bytes(), arrivals); err != nil {
+		log.Print(err)
+	}
+
+	fmt.Printf("%+v", arrivals)
+
+	return arrivals, nil
 }
