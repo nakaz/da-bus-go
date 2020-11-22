f1280c8ffd70e969744ed0b2fe6f1489f7523918
Sean Nakamura <seanenakamura@gmail.com>
2020-11-22 13:16:17 -0800

    Initial fetchVehicle api call

--- thebus.go
+++ thebus.go
@@ -36,6 +36,23 @@ type Arrivals struct {
 	} `json:"stopTimes"`
 }
 
+type Vehicle struct {
+	Vehicles struct {
+		Timestamp string `json:"timestamp"`
+		Vehicle   struct {
+			Driver         string `json:"driver"`
+			Longitude      string `json:"longitude"`
+			LastMessage    string `json:"last_message"`
+			Headsign       string `json:"headsign"`
+			RouteShortName string `json:"route_short_name"`
+			Number         string `json:"number"`
+			Trip           string `json:"trip"`
+			Latitude       string `json:"latitude"`
+			Adherence      string `json:"adherence"`
+		} `json:"vehicle"`
+	} `json:"vehicles"`
+}
+
 func fetchArrivals(s string) (*Arrivals, error) {
 	arrivalsPath := fmt.Sprintf("http://api.thebus.org/arrivals/?key=%v&stop=%v", apiKey, s)
 	resp, err := http.Get(arrivalsPath)
@@ -64,4 +81,34 @@ func fetchArrivals(s string) (*Arrivals, error) {
 	}
 
 	return arrivals, nil
+}
+
+func fetchVehicle(s string) (*Vehicle, error) {
+	vehiclePath := fmt.Sprintf("http://api.thebus.org/vehicle/?key=%v&num=%v", apiKey, s)
+	resp, err := http.Get(vehiclePath)
+	if err != nil {
+		log.Print(err)
+	}
+	defer resp.Body.Close()
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	respString := string(respData)
+	xmlString := strings.NewReader(respString)
+
+	vehicle := &Vehicle{}
+
+	jsonResp, err := xj.Convert(xmlString)
+	if err != nil {
+		log.Print(err)
+	}
+
+	if err := json.Unmarshal(jsonResp.Bytes(), vehicle); err != nil {
+		log.Print(err)
+	}
+
+	return vehicle, nil
 }
