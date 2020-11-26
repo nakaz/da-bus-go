790e86e248ee3bafa83c0a01650ca4c262f37587
Sean Nakamura <seanenakamura@gmail.com>
2020-11-25 16:48:15 -0800

    Ensure values to satisfy pointers

--- schema.go
+++ schema.go
@@ -2,21 +2,21 @@ package main
 
 const rootSchema = `
 type Query {
-		arrivals (stop: String!): [Arrival]
+		arrivals (stop: String!): [Arrival]!
 }
 type Arrival {
-		headsign:  String
-		stoptime:  String
-		vehicle:   String
-		estimated: String
-		latitude:  String
-		longitude: String
-		shape:     String
-		id:        String
-		trip:      String
-		canceled:  String
-		date:      String
-		route:     String
-		direction: String
+		headsign:  String!
+		stoptime:  String!
+		vehicle:   String!
+		estimated: String!
+		latitude:  String!
+		longitude: String!
+		shape:     String!
+		id:        String!
+		trip:      String!
+		canceled:  String!
+		date:      String!
+		route:     String!
+		direction: String!
 }
 `
